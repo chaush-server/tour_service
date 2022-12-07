ab25d7d4534b9a5a586c82b79fbf80df1555a04e
Server <chaush.s.i20@gmail.com>
2022-12-07 22:59:31 +0300

    tour_service

--- app.py
+++ app.py
@@ -1,57 +1,104 @@
 # Store this code in 'app.py' file
 
 from flask import Flask, render_template, request, redirect, url_for, session
-from flask_mysqldb import MySQL
-import MySQLdb.cursors
 import re
+import sqlite3
 from passlib.hash import pbkdf2_sha256
 
 app = Flask(__name__)
 
-app.secret_key = 'M-s-i-k'
-
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'tour_service'
-
-mysql = MySQL(app)
+app.secret_key = 'M-s-i-kgh#$245rK:?/;!/?!/42-2'
 
 
-@app.route('/home/')
 @app.route('/home')
 def home():
+    db = sqlite3.connect("tour_service.db")
+    db.row_factory = sqlite3.Row
+    cursor = db.cursor()
     if session.get('logged'):
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM service')
         data = cursor.fetchall()
+        print(data)
         return render_template('home.html', data=data)
     else:
         return redirect("/login")
 
 
-@app.route('/home/service/<int:service_id>')
+@app.route('/find=<string:name>')
+def home_find(name):
+    db = sqlite3.connect("tour_service.db")
+    db.row_factory = sqlite3.Row
+    cursor = db.cursor()
+    if session.get('logged'):
+        cursor.execute(f"""SELECT * FROM service WHERE name LIKE '%{name}%'""")
+        data = cursor.fetchall()
+        if data:
+            print(data)
+            return render_template('home.html', data=data)
+        else:
+            return render_template('home.html')
+    else:
+        return redirect("/login")
+
+
+@app.route('/service/<int:service_id>')
 def service(service_id):
-    return str(service_id)
+    db = sqlite3.connect("tour_service.db")
+    db.row_factory = sqlite3.Row
+    cursor = db.cursor()
+    cursor.execute(f'SELECT * FROM service WHERE service_id = {service_id}')
+    data = cursor.fetchone()
+    return render_template('service.html', data=data)
+
+
+@app.route('/cart/<int:service_id>')
+def buy(service_id):
+    db = sqlite3.connect("tour_service.db")
+    db.row_factory = sqlite3.Row
+    cursor = db.cursor()
+    cursor.execute(f'SELECT * FROM service WHERE service_id = {service_id}')
+    data = cursor.fetchone()
+    print(data)
+    return render_template("cart.html", data=data)
+
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
+
+@app.route('/cart/<int:service_id>')
+def buy_service(service_id):
+    db = sqlite3.connect("tour_service.db")
+    db.row_factory = sqlite3.Row
+    cursor = db.cursor()
+    cursor.execute(f'SELECT * FROM service WHERE service_id = {service_id}')
+    data = cursor.fetchone()
+    return render_template('cart.html', data=data)
 
 
 @app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
-    msg = 'Hello'
+    db = sqlite3.connect("tour_service.db")
+    db.row_factory = sqlite3.Row
+    cursor = db.cursor()
+    msg = ''
     if session.get('logged'):
         return redirect('/home')
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM user WHERE username = % s', (username,))
+        cursor.execute(f"SELECT * FROM user WHERE username = {username}")
         account = cursor.fetchone()
         if account and pbkdf2_sha256.verify(request.form['password'], account['password']):
+            if account['admin']:
+                session['admin'] = True
+            else:
+                session['admin'] = False
             session['logged'] = True
             session['id'] = account['user_id']
             session['username'] = account['username']
-            msg = 'Logged in successfully !'
             return redirect('/home')
         else:
             msg = 'Incorrect username / password !'
@@ -68,6 +115,9 @@ def logout():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    db = sqlite3.connect("tour_service.db")
+    db.row_factory = sqlite3.Row
+    cursor = db.cursor()
     if session.get('logged'):
         return redirect('/home')
     msg = ''
@@ -76,8 +126,7 @@ def register():
         username = request.form['username']
         password = pbkdf2_sha256.hash(request.form['password'])
         email = request.form['email']
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM user WHERE username = % s', (username,))
+        cursor.execute(f"SELECT * FROM user WHERE username = {username}")
         account = cursor.fetchone()
         if account:
             msg = 'Account already exists !'
@@ -89,8 +138,7 @@ def register():
             msg = 'Please fill out the form !'
         else:
             cursor.execute('INSERT INTO user VALUES (NULL, % s, % s, % s)', (username, password, email,))
-            mysql.connection.commit()
-            msg = 'You have successfully registered !'
+            db.commit()
             return redirect('/login')
     elif request.method == 'POST':
         msg = 'Please fill out the form !'
